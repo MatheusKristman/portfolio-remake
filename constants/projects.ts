7876e91c19ca08ff740913814ec1278a64429236
MatheusKristman <kristman058@gmail.com>
2025-03-28 01:31:17 -0300

    added cp vistos to projects page

--- constants/projects.ts
+++ constants/projects.ts
@@ -36,6 +36,42 @@ export const projects = [
       "Tendo como objetivo proporcionar uma experiência aprimorada aos meus clientes, reformulei completamente meu site de freelancer. A nova versão oferece uma navegação intuitiva, um design moderno e informações mais claras sobre meus serviços e portfólio. Com ênfase na usabilidade e na apresentação visual, garantindo que os visitantes tenham uma visão clara do que ofereço e do valor que posso agregar aos seus projetos.",
   },
   {
+    projectName: "CP Vistos",
+    techs: [
+      {
+        lightImageUrl: "/assets/icons/nextjs-light.svg",
+        darkImageUrl: "/assets/icons/nextjs-dark.svg",
+        content: "Next.js",
+      },
+      {
+        lightImageUrl: "/assets/icons/typescript-light.svg",
+        darkImageUrl: "/assets/icons/typescript-dark.svg",
+        content: "Typescript",
+      },
+      {
+        lightImageUrl: "/assets/icons/tailwindcss-light.svg",
+        darkImageUrl: "/assets/icons/tailwindcss-dark.svg",
+        content: "TailwindCSS",
+      },
+      {
+        lightImageUrl: "/assets/icons/mongodb-light.svg",
+        darkImageUrl: "/assets/icons/mongodb-dark.svg",
+        content: "MongoDB",
+      },
+      {
+        lightImageUrl: "/assets/icons/figma-light.svg",
+        darkImageUrl: "/assets/icons/figma-dark.svg",
+        content: "Figma",
+      },
+    ],
+    service: "Freelancer",
+    githubLink: "https://github.com/MatheusKristman/cp-vistos",
+    projectLink: "https://www.cpvistos.com.br/",
+    projectImageUrl: "/assets/images/cp-vistos.png",
+    projectDesc:
+      "CP Vistos é uma agência especializada em assessoria para emissão de vistos americanos e passaportes. Com 18 anos de experiência, a agência já auxiliou milhares de clientes, garantindo um processo mais seguro e eficiente. Além do suporte completo para obtenção do visto, a CP Vistos oferece treinamento personalizado para que seus clientes se preparem para a entrevista consular, aumentando as chances de aprovação.",
+  },
+  {
     projectName: "O Sapiente",
     techs: [
       {
@@ -66,46 +102,10 @@ export const projects = [
     ],
     service: "Freelancer",
     githubLink: "https://github.com/MatheusKristman/o-sapiente",
-    projectLink: "https://o-sapiente-91ku.vercel.app/",
+    projectLink: "https://www.osapiente.com/",
     projectImageUrl: "/assets/images/o-sapiente.png",
     projectDesc:
       "O Sapiente é uma plataforma de ensino, criada para facilitar a interação entre professores e alunos. Com recursos de chat integrados, os professores podem ministrar aulas diretamente aos alunos, proporcionando uma experiência de aprendizado personalizada e interativa. Além disso, os alunos têm a facilidade de fazer solicitações dentro do próprio site para encontrar professores qualificados para suas necessidades específicas. Essa plataforma visa promover o acesso à educação de qualidade, conectando professores talentosos com alunos em busca de conhecimento.",
-  },
-  {
-    projectName: "Chattie",
-    techs: [
-      {
-        lightImageUrl: "/assets/icons/nextjs-light.svg",
-        darkImageUrl: "/assets/icons/nextjs-dark.svg",
-        content: "Next.js",
-      },
-      {
-        lightImageUrl: "/assets/icons/typescript-light.svg",
-        darkImageUrl: "/assets/icons/typescript-dark.svg",
-        content: "Typescript",
-      },
-      {
-        lightImageUrl: "/assets/icons/tailwindcss-light.svg",
-        darkImageUrl: "/assets/icons/tailwindcss-dark.svg",
-        content: "TailwindCSS",
-      },
-      {
-        lightImageUrl: "/assets/icons/mongodb-light.svg",
-        darkImageUrl: "/assets/icons/mongodb-dark.svg",
-        content: "MongoDB",
-      },
-      {
-        lightImageUrl: "/assets/icons/figma-light.svg",
-        darkImageUrl: "/assets/icons/figma-dark.svg",
-        content: "Figma",
-      },
-    ],
-    service: "Freelancer",
-    githubLink: "https://github.com/MatheusKristman/chattier",
-    projectLink: "https://chattier.vercel.app/",
-    projectImageUrl: "/assets/images/chattie.png",
-    projectDesc:
-      "Chattie é uma plataforma de chat desenvolvida como parte de um projeto de treinamento em integração com Web Socket. Esta plataforma permite que os usuários troquem mensagens em tempo real de forma fácil e eficiente. Com uma interface intuitiva, Chattie oferece uma experiência de chat moderna e dinâmica. Este projeto visa aprimorar habilidades técnicas, especialmente na área de comunicação em tempo real, enquanto oferece aos usuários uma maneira simples e prática de se conectar e comunicar online.",
   },
   {
     projectName: "Hanazaki Studio",
